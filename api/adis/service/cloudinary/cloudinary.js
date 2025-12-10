edc71f1e0ff9839850c2978fb1c0e1ba4c4194c1
Adistian Herlambang <adistian59@gmail.com>
2025-12-11 01:02:44 +0700

    cloud

--- api/adis/service/cloudinary/cloudinary.js
+++ api/adis/service/cloudinary/cloudinary.js
@@ -2,28 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cloudinary = require("./config");
 
-// GET: read list file dari cloudinary (misalnya folder "my_folder")
-// router.get("/cloudinary", async (req, res) => {
-//   try {
-//     const { resources } = await cloudinary.search
-//       .expression('folder:my_folder') // ganti dengan folder kamu
-//       .sort_by('public_id', 'desc')
-//       .max_results(50)
-//       .execute();
-
-//     res.json({
-//       message: "Success read from Cloudinary",
-//       data: resources,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({
-//       error: "Failed read Cloudinary",
-//     });
-//   }
-// });
-
-router.get("/cloudinary", async (req, res) => {
+router.get("/", async (req, res) => {
   const url = cloudinary.url('main-sample')
   res.send(url)
 })
