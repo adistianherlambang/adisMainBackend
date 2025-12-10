600f6ba8bb7f16628c18e61c39c1b397e320a799
Adistian Herlambang <adistian59@gmail.com>
2025-12-10 18:48:41 +0700

    vercel

--- api/main.js
+++ api/main.js
@@ -4,6 +4,7 @@ const serverless = require("serverless-http");
 const app = express();
 app.use(express.json());
 
+// ROUTES
 app.get("/", (req, res) => {
   res.json({ message: "Main API OK" });
 });
@@ -12,6 +13,14 @@ app.get("/test", (req, res) => {
   res.json({ message: "Main API Test OK" });
 });
 
-// penting!
+// LOCAL DEVELOPMENT
+if (process.env.NODE_ENV === "development") {
+  const port = 5000;
+  app.listen(port, () => {
+    console.log(`Server running locally on http://localhost:${port}`);
+  });
+}
+
+// EXPORT FOR VERCEL
 module.exports = app;
 module.exports.handler = serverless(app);
