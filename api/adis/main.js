f172d2a7527013d102915edff1c743b17bdd7e29
Adistian Herlambang <adistian59@gmail.com>
2025-12-14 09:30:52 +0700

    server

--- api/adis/main.js
+++ api/adis/main.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const cloudinaryServices = require("./service/cloudinary/cloudinary")
-const waclient = require("./service/whatsapp-client/main.js")
+// const waclient = require("./service/whatsapp-client/main.js")
 
 router.use("/cloud", cloudinaryServices)
-router.use("/wa", waclient)
+// router.use("/wa", waclient)
 
 router.get("/", (req,res) => {
   res.json("adis")
